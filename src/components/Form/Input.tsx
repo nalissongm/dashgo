026baef38e238777231490742283f0d5ebded4db
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-04-13 15:44:37 -0300

    feat: implementing dashboard and user listing page

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,26 @@
+import { FormLabel, FormControl, Input as ChackraInput, InputProps as ChackraInputProps } from '@chakra-ui/react';
+
+interface InputProps extends ChackraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({name, label, ...rest}: InputProps){
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>E-mail</FormLabel>}
+      <ChackraInput
+        id={name} 
+        name={name}
+        focusBorderColor='pink.500'
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        {...rest}
+      /> 
+    </FormControl>
+  );
+}
