63e74e05b2e8d55dd2ceeb66bf5b21fd206c7df2
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-04-14 22:27:50 -0300

    refactor(Sidebar): componentizing elements

---
+++ src/components/Sidebar/NavLink.tsx
@@ -0,0 +1,16 @@
+import { Icon, Link, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import { ElementType } from "react";
+
+interface NavLinkProps extends ChakraLinkProps {
+  icon: ElementType;
+  children: string;
+}
+
+export function NavLink({icon, children, ...rest}: NavLinkProps): JSX.Element {
+  return (
+    <Link display="flex" alignItems="center" { ...rest }>
+      <Icon as={icon} fontSize="20" />
+      <Text ml="4" fontWeight="medium">{children}</Text>
+    </Link>
+  );
+}
