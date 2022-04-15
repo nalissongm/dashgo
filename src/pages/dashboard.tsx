fa4be330ddebfcdaf728268d211d84120d2754f2
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-04-15 02:42:18 -0300

    style: reducing padding on smaller screens

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -69,7 +69,7 @@ export default function Dashboard() {
         <Sidebar />
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" >
           <Box
-            p="8"
+            p={["6","8"]}
             bgColor="gray.800"
             borderRadius={8}
             pb="4"
@@ -78,7 +78,7 @@ export default function Dashboard() {
             <Chart options={options} series={series} type="area" height={180}/>
           </Box>
           <Box
-            p="8"
+            p={["6","8"]}
             bgColor="gray.800"
             borderRadius={8}
           >
